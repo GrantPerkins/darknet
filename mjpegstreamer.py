ef59c28ec1fa46a19ffd3e189b8acb518adc2839
GrantPerkins <gcperk20@gmail.com>
2019-05-31 16:55:15 -0400

    Document mjpegstreamer

--- mjpegstreamer.py
+++ mjpegstreamer.py
@@ -1,20 +1,33 @@
 import cv2
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
-import time
-from urllib.request import urlopen
 
 
-class MJPGServer:
+class MJPEGServer:
+    """
+    The main MJPEG server class. Interfaces with the other classes in this thread.
+    """
     def __init__(self, ip):
+        """
+        Constructs an MJPEGServer object
+        :param ip:
+        """
         self.frame = None
         self._has_started = False
         self._ip = ip
 
     def started(self):
+        """
+        Gets state of server
+        :return: whether server has started
+        """
         return self._has_started
 
     def start(self, port):
+        """
+        Starts MJPEG server. Should be done inside a thread.
+        :param port: port to stream to. Use 8190, because team 190 is cool.
+        """
         self._has_started = True
         CamHandler.set_capture(self)
         with ThreadedHTTPServer((self._ip, int(port)), CamHandler) as server:
@@ -24,21 +37,32 @@ class MJPGServer:
             server.serve_forever()
 
     def send_image(self, img):
+        """
+        Update the MJPEG stream with current image
+        :param img: New image to put
+        """
         self.frame = img
 
     def get_image(self):
+        """
+        Gets current image
+        :return: Current image
+        """
         return self.frame
-
-    def ask_mjpeg(self, ip, port):
-        time.sleep(5)
-        while 1:
-            urlopen("http://" + ip + ":" + port + "/").read()
 
 
 class CamHandler(BaseHTTPRequestHandler):
-    capture = None
+    """
+    Handles the get requests to the server. Overrides do_GET(). Just pushes current image to stream.
+    """
+
+    capture = None # MJPEGServer object, used to get frames.
 
     def do_GET(self):
+        """
+        Deals with get requests. Puts current frame.
+        :return: None
+        """
         self.send_response(200)
         self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=jpgboundary')
         self.end_headers()
@@ -56,6 +80,10 @@ class CamHandler(BaseHTTPRequestHandler):
 
     @staticmethod
     def set_capture(capture):
+        """
+        Sets static capture variable. Should be the MJPEGServer.
+        :param capture: MJPEGServer
+        """
         CamHandler.capture = capture
 
 
